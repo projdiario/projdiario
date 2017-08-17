ab67d8e88b965dfb9600b664c9e7190f2881dd49
Luisa Teixeira Puppin Gonçalves <luisa.tpg@outlook.com>
2017-08-17 13:53:53 -0300

    Conversor PDF (Versão 3.0)
    Nova metodologia de inserção de tags em tabelas. Colspan e Rowspan.

--- python/converter.py
+++ python/converter.py
@@ -37,7 +37,6 @@ import os
 import re
 import sys
 import time
-from statistics import mode
 import pywintypes
 import paths
 import avoider
@@ -45,86 +44,29 @@ import win32com.client as win32
 
 # Word App
 WORD = win32.gencache.EnsureDispatch('Word.Application')
-WORD.Visible = False
+WORD.Visible = True
 TXT_FORMAT = win32.constants.wdFormatText
+WORD.DisplayAlerts = win32.constants.wdAlertsNone
 
 # Def
-def dict_col_width(sum_cols, sum_rows, table):
-    """
-    Create a dictionary with the integer mode width for all columns in table.
-    Format: {column_number: [mode_width],
-             column_number: [mode_width],
-             (...)}
-    Arguments:
-        1. sum_col: Sum of all columns in the desired table.
-        2. sum_row: Sum of all rows in the desired table.
-        3. table: Number of the active table in Word.
-    """
-    col_width = {}
-    for col_n in range(1, sum_cols+1):
-        col_nor = []
-        for row_n in range(1, sum_rows+1):
-            try:
-                width_a = active_doc.Tables(table).Cell(Row=row_n, Column=col_n).Width
-                col_nor.append(int(width_a))
-            except:
-                width_a = 0
-                col_nor.append(width_a)
-        col_nor = [item for item in col_nor if item != 0]
-        try:
-            col_nor = mode(col_nor)
-        except:
-            col_nor = min(col_nor)
-        col_width[col_n] = col_nor
-    return col_width
+class AtributteTables(object):
+    def __init__(self, num_table, qtd_col, qtd_row):
+        self.num_table = num_table
+        self.qtd_col = qtd_col
+        self.qtd_row = qtd_row
 
-def merged_cells(sum_cols, sum_rows, table):
+def get_width(table_gw, row_gw, col_gw):
+    """ Get the width of each cell (row, col).
+    Atributes:
+        1. table_gw: Number of the table.
+        2. row_gw: Number of the row.
+        3. col_gw: Number of the Column.
     """
-    Create a dictionary with all integer widths from the lines with merged cells.
-    Format: {row_with_merged_cells: [col1_width, col2_width, col3_width],
-             row_with_merged_cells: [col1_width, col2_width, col3_width],
-             (...)}
-    Arguments:
-        1. sum_col: Sum of all columns in the desired table.
-        2. sum_row: Sum of all rows in the desired table.
-        3. table: Number of the active table in Word.
-    """
-    dict_width = {}
-    for row_nu in range(1, sum_rows+1):
-        widths_row = []
-        for col_nu in range(1, sum_cols+1):
-            try:
-                width_b = int(active_doc.Tables(table).Cell(Row=row_nu, Column=col_nu).Width)
-                widths_row.append(width_b)
-            except:
-                width_b = 0
-                widths_row.append(width_b)
-        if 0 in widths_row:
-            dict_width[row_nu] = widths_row
-        else:
-            pass
-    return dict_width
-
-def table_tags(row, col, table, sum_rows, sum_cols):
-    """
-    Insert HTML table tags (table, tr and td) in the selected cell.
-    Arguments:
-        1. row: Number of the row where the tag will be inserted.
-        2. column: Number of the column where the tag will be inserted.
-        3. table: Number of the active table in Word.
-        4. sum_col: Sum of all columns in the active table.
-        5. sum_row: Sum of all rows in the active table.
-    """
-    active_doc.Tables(table).Cell(Row=row, Column=col).Range.InsertBefore("<td>")
-    active_doc.Tables(table).Cell(Row=row, Column=col).Range.InsertAfter("</td>")
-    if col == 1:
-        active_doc.Tables(table).Cell(Row=row, Column=1).Range.InsertBefore("<tr>")
-        if row == 1:
-            active_doc.Tables(table).Cell(Row=1, Column=1).Range.InsertBefore("<table>")
-    if col == sum_cols:
-        active_doc.Tables(table).Cell(Row=row, Column=sum_col).Range.InsertAfter("</tr>")
-        if row == sum_rows:
-            active_doc.Tables(table).Cell(Row=sum_rows, Column=sum_cols).Range.InsertAfter("</table>")
+    try:
+        wc = int(active_doc.Tables(table_gw).Cell(Row=row_gw, Column=col_gw).Width)
+    except:
+        wc = 0
+    return wc
 
 def join_inner_list(lst_to_join):
     """ Join list inside of list to form only one list.
@@ -173,6 +115,29 @@ def log_writer(logfile_path, *args_to_write, sep=";", end_line=None):
             with open(logfile_path, 'a') as log_control:
                 log_control.write(log_line)
 
+def row_to_col(lst_rows, num_cols):
+    len_dict_row = len(lst_rows)
+    dict_col2 = []
+    for cn in range(num_cols):
+        col_atr = []
+        for rn in range(len_dict_row):
+            try:
+                col_atr.append(lst_rows[rn][cn])
+            except:
+                col_atr.append(0)
+        dict_col2.append(col_atr)
+    return dict_col2
+
+def col_to_row(lst_cols, num_rows):
+    len_dict_cols = len(lst_cols)
+    dict_row2 = []
+    for rn in range(num_rows):
+        row_atr = []
+        for cn in range(len_dict_cols):
+            row_atr.append(lst_cols[cn][rn])
+        dict_row2.append(row_atr)
+    return dict_row2
+
 # Count PDF conversion list
 PDF_CONVERTION_COUNT = len(avoider.PDF_CONVERSION_LIST)
 
@@ -183,23 +148,19 @@ LOG_FILE = os.path.join(paths.LOG_DIR, 'log_'+TIME_LOG_FILE+'.txt')
 # Create PDF, TXT and HTML path list
 PDF_PATH_LIST = []
 TXT_PATH_LIST = []
-# HTML_PATH_LIST = []
 
 # Path creation loop
 for i in range(PDF_CONVERTION_COUNT):
     FILENAME = avoider.PDF_CONVERSION_LIST[i]
     PDF_PATH = os.path.join(paths.PDF_DIR, FILENAME+'.pdf')
     TXT_PATH = os.path.join(paths.TXT_DIR, FILENAME+'.txt')
-    # HTML_PATH = os.path.join(paths.HTML_DIR, FILENAME+'.html')
     PDF_PATH_LIST.append(PDF_PATH)
     TXT_PATH_LIST.append(TXT_PATH)
-    # HTML_PATH_LIST.append(HTML_PATH)
 
 # Count PDF conversion list
 PDF_PATH_COUNT = len(PDF_PATH_LIST)
 PDF_PATH_LIST = [w.replace('\\', '\\\\') for w in PDF_PATH_LIST]
 TXT_PATH_LIST = [w.replace('\\', '\\\\') for w in TXT_PATH_LIST]
-# HTML_PATH_LIST = [w.replace('\\', '\\\\') for w in HTML_PATH_LIST]
 
 # Conversion Loop TXT
 for n in range(PDF_PATH_COUNT):
@@ -227,7 +188,7 @@ for n in range(PDF_PATH_COUNT):
             index_agri = mapa_index(sumario_split)
             mapa_start = int(sumario_split[index_agri+1])
             mapa_end = int(sumario_split[index_agri+3])
-            print(mapa_start, mapa_end)
+            print(FILENAME, mapa_start, mapa_end)
         except:
             pass
 
@@ -237,14 +198,24 @@ for n in range(PDF_PATH_COUNT):
             mapa_pdf_path = os.path.join(paths.PDF_DIR, mapa_pdf+'.pdf')
             mapa_txt_path = os.path.join(paths.TXT_DIR, mapa_pdf+'.txt')
             LIST_REAL_TABLES = []
+            TIME_CONVERSION = time.strftime('%H:%M:%S')
             try:
                 FILENAME = mapa_pdf
+                print(FILENAME)
                 FILEPATH = mapa_pdf_path
                 TXTPATH = mapa_txt_path
                 WORD.Documents.Open(FILEPATH)
                 active_doc = WORD.ActiveDocument
+
+                active_doc.ShowGrammaticalErrors = False
+                active_doc.ShowSpellingErrors = False
+                active_doc.Paragraphs.LineSpacingRule = win32.constants.wdLineSpaceSingle
+                active_doc.Paragraphs.SpaceBefore = 0
+                active_doc.Paragraphs.SpaceAfter = 0
+
                 ALL_TABLES = active_doc.Tables.Count
                 for i in range(1, ALL_TABLES+1):
+                    active_doc.Tables(i).Columns.PreferredWidth = 50
                     sum_col = active_doc.Tables(i).Columns.Count
                     sum_row = active_doc.Tables(i).Rows.Count
                     sum_cell = active_doc.Tables(i).Range.Cells.Count
@@ -263,39 +234,182 @@ for n in range(PDF_PATH_COUNT):
                     LIST_REAL_TABLES = list(set(LIST_REAL_TABLES))
                 except:
                     LIST_REAL_TABLES = []
-                if len(LIST_REAL_TABLES) > 0:
-                    for i in LIST_REAL_TABLES:
-                        sum_col = active_doc.Tables(i).Columns.Count
-                        sum_row = active_doc.Tables(i).Rows.Count
-                        sum_cell = active_doc.Tables(i).Range.Cells.Count
-                        if sum_cell < (sum_col * sum_row):
-                            def_width = dict_col_width(sum_col, sum_row, i)
-                            target_rows = merged_cells(sum_col, sum_row, i)
-                            for j in range(1, sum_row+1):
-                                if j in target_rows:
-                                    for k in range(1, sum_col+1):
-                                        try:
-                                            width = target_rows[j][k-1]
-                                            if width > def_width[k]:
-                                                active_doc.Tables(i).Cell(Row=j, Column=k).Split(NumRows=1, NumColumns=2)
-                                                table_tags(j, k, i, sum_row, sum_col)
-                                                target_rows[j].insert(k, def_width[k+1])
-                                                target_rows[j].remove(0)
+
+                for i in LIST_REAL_TABLES:
+
+                    act_table = active_doc.Tables(i)
+                    qtd_linha = act_table.Rows.Count
+                    qtd_coluna = act_table.Columns.Count
+                    atr = AtributteTables(i, qtd_coluna, qtd_linha)
+                    dict_row = []
+                    for j in range(1, atr.qtd_row+1):
+                        lst_row = []
+                        for k in range(1, atr.qtd_col+1):
+                            tam = get_width(i, j, k)
+                            lst_row.append(tam)
+                        dict_row.append(lst_row)
+
+
+                    matriz = []
+                    word_col = 0
+                    float_cols = atr.qtd_col
+
+                    while word_col < float_cols:
+
+                        add_float = True
+
+                        matriz_da_coluna = []
+
+                        teste = row_to_col(dict_row, float_cols)
+                        coluna = teste[word_col]
+
+                        # Achar o tamanho mínimo maior que zero da coluna!
+                        # Try para ignorar tabelas que ganham colunas transitórias.
+                        try:
+                            sem_zero = [x for x in coluna if x != 0]
+                            minimo = min(sem_zero)
+
+                            # Rodar célula a célula na coluna
+                            for linha in range(atr.qtd_row):
+                                celula = coluna[linha]
+                                if celula == minimo:
+                                    matriz_da_coluna.append('<td>')
+
+                                elif celula > minimo:
+                                    if celula == 66666666:
+                                        matriz_da_coluna.append(matriz[word_col-1][linha])
+
+                                    else:
+
+                                        cont_rowspan = 2
+                                        add_cell = 1
+                                        soma_rowspan = minimo
+                                        see_next_col = True
+
+                                        while see_next_col is True:
+
+                                            if word_col+add_cell > float_cols-1:
+                                                see_next_col = False
+
                                             else:
-                                                table_tags(j, k, i, sum_row, sum_col)
-                                        except:
-                                            try:
-                                                active_doc.Tables(i).Cell(Row=j-1, Column=k).Split(NumRows=2, NumColumns=1)
-                                                table_tags(j, k, i, sum_row, sum_col)
-                                            except:
-                                                pass
-                                else:
-                                    for k in range(1, sum_col+1):
-                                        table_tags(j, k, i, sum_row, sum_col)
-                        else:
-                            for j in range(1, sum_row+1):
-                                for k in range(1, sum_col+1):
-                                    table_tags(j, k, i, sum_row, sum_col)
+
+                                                celula_retirada = teste[word_col+add_cell].pop(linha)
+
+                                                # Achar o tamanho mínimo maopr que zero da próxima coluna
+                                                proxima_col_sem_zero = [x for x in teste[word_col+add_cell] if x != 0]
+                                                minimo_proxima_col = min(proxima_col_sem_zero)
+
+                                                teste[word_col+add_cell].insert(linha, celula_retirada)
+
+                                                if soma_rowspan + minimo_proxima_col in range(celula-2, celula+3):
+                                                    see_next_col = False
+
+                                                else:
+                                                    cont_rowspan += 1
+                                                    add_cell += 1
+                                                    soma_rowspan += minimo_proxima_col
+
+                                            text_rowspan = '<td colspan="' + str(cont_rowspan) + '">'
+
+                                        matriz_da_coluna.append(text_rowspan)
+                                        dict_row[linha][word_col] = minimo
+
+                                    # Modifica o dado da base da linha para corrigir o erro das próximas comparações!
+                                        for ajst_rowspan in range(1, cont_rowspan):
+                                            dict_row[linha].insert(word_col+ajst_rowspan, 66666666)
+                                            if dict_row[linha][-1] == 0 and float_cols == atr.qtd_col:
+                                                dict_row[linha] = dict_row[linha][:-1]
+                                            else:
+                                                if add_float is True:
+                                                    float_cols += 1
+                                                    add_float = False
+
+                                else:        # Celula é mesclada verticalmente
+                                    if coluna[linha-1] == 0:
+                                        matriz_da_coluna.append(matriz_da_coluna[linha-1])
+                                    else:
+                                        cont_colspan = 2
+                                        add_cell = 1
+                                        see_next_cell = True
+                                        while see_next_cell is True:
+                                            index_next = linha + add_cell
+                                            if index_next > atr.qtd_row-1:
+                                                see_next_cell = False
+                                            elif coluna[index_next] == 0:
+                                                cont_colspan += 1
+                                                add_cell += 1
+                                            else:
+                                                see_next_cell = False
+                                            text_colspan = '<td rowspan="' + str(cont_colspan) + '">'
+                                        matriz_da_coluna.append(text_colspan)
+                                        matriz_da_coluna[linha-1] = text_colspan
+
+                            matriz.append(matriz_da_coluna)
+                            word_col += 1
+                        
+                        except:
+                            word_col += 1
+
+                    tags_to_rows = col_to_row(matriz, atr.qtd_row)
+                    len_tags_rows = len(tags_to_rows)
+                    for k in range(len_tags_rows):
+                        rows_tags = tags_to_rows[k]
+                        len2 = len(tags_to_rows[k])
+                        delete_itens = []
+                        for j in range(len2):
+                            try:
+                                if 'colspan' in rows_tags[j]:
+                                    del_range = [int(x) for x in rows_tags[j].split(sep='"') if x.isdigit()]
+                                    cont = 1
+                                    while cont < del_range[0]:
+                                        del[rows_tags[j+1]]
+                                        cont += 1
+                            except:
+                                pass
+                            
+                    tr_index = []
+                    for k in range(atr.qtd_row):
+                        tr_row = dict_row[k]
+                        tr_len = len(tr_row)
+                        add_tr = 0
+                        sub_tr = -1
+                        check_next = True
+                        check_before = True
+                        tr_beg_end = []
+                        while check_next is True:
+                            tr_cell = tr_row[add_tr]
+                            if tr_cell == 0:
+                                add_tr += 1
+                            else:
+                                tr_beg_end.append(add_tr+1)
+                                check_next = False
+                        while check_before is True:
+                            tr_end = [x for x in tr_row if x != 66666666]
+                            tr_cell = tr_end[sub_tr]
+                            if tr_cell == 0:
+                                sub_tr -= 1
+                            else:
+                                tr_beg_end.append(len(tr_end)+sub_tr+1)
+                                check_before = False
+                        tr_index.append(tr_beg_end)
+
+                    for k in range(1, atr.qtd_row+1):
+                        for j in range(1, atr.qtd_col+1):
+                            try:
+                                act_table.Cell(Row=k, Column=j).Range.InsertBefore(tags_to_rows[k-1][j-1])
+                                act_table.Cell(Row=k, Column=j).Range.InsertAfter('</td>')
+                                if j == tr_index[i-1][0]:
+                                    act_table.Cell(Row=k, Column=j).Range.InsertBefore('<tr>')
+                                    if k == 1:
+                                        act_table.Cell(Row=k, Column=j).Range.InsertBefore('<table>')
+                                elif j == tr_index[k-1][1]:
+                                    act_table.Cell(Row=k, Column=j).Range.InsertAfter('</tr>')
+                                    if k == atr.qtd_row:
+                                        act_table.Cell(Row=k, Column=j).Range.InsertAfter('</table><br>')
+
+                            except:
+                                pass
+
                 active_doc.SaveAs(TXTPATH, FileFormat=TXT_FORMAT)
                 active_doc.Close(False)
 
