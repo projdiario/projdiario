fbea3a3eee523dae4f77f27c372b9eb602fec7f7
projdiario <tomas.barcellos@agricultura.gov.br>
2017-09-20 14:47:16 -0300

    Usa "bind variables" no SQL que escreve os registros na base

--- R/validador/global.R
+++ R/validador/global.R
@@ -44,7 +44,7 @@ jsCode <<- "shinyjs.pegaTexto = function() {
             };"
 
 # Começa a brincadeira
-normas <- readRDS('aplicacao.RDS')
+normas <- readRDS('www/aplicacao.RDS')
 
 if (file.exists('log.RDS')) {
   lidos <- readRDS('log.RDS')
@@ -59,16 +59,18 @@ if (file.exists('log.RDS')) {
 
 escrever_na_base <- function(input, driver, configs) {
   conexao <- RJDBC::dbConnect(driver, configs[1], configs[2], configs[3])
+  
   NUM_ATO <- formatC(input$num_norma, width = 8, flag = '0')
   SGL_TIPO <- input$sgl_tipo
   VLR_ANO <- as.character(lubridate::year(input$data_dou))
   SGL_ORGAO <- input$sgl_orgao
-  # COD_TIPO <- ''
-  # DES_ITEM <- ''
-  # NUM_LINHA <- 0
+  COD_TIPO <- 'A' # Precisa determinar
+  DES_ITEM <- '0' # Precisa determinar
+  NUM_LINHA <- '0' # Precisa determinar
   TXT_TEXTO <- input$entradaPrincipal
   DTA_PROMULGACAO <- input$data_dou
   TXT_EMENTA <- paste0("<p>", input$txt_ementa, "</p>")
+  COD_LOCAL <- 'BR' # Precisa determinar
   DES_TITULO <- input$des_titulo
   
   query_seq <- paste0("SELECT * FROM ADMLEGIS.ATO_AGRICULTURA WHERE NUM_ATO='",
@@ -76,33 +78,33 @@ escrever_na_base <- function(input, driver, configs) {
                       VLR_ANO, "' AND SGL_ORGAO='", SGL_ORGAO, "'")
   
   registros_seq <- RJDBC::dbGetQuery(conexao, query_seq)
+  
   SEQ_ATO <- max(as.numeric(registros_seq$SEQ_ATO))
+  
   if (is.infinite(SEQ_ATO) | is.na(SEQ_ATO)) {
     SEQ_ATO <- '000'
   } else {
     SEQ_ATO <- formatC(SEQ_ATO + 1, width = 3, flag = '0')
   }
   
-  query1 <- paste0("DECLARE TEXTO CLOB := '", TXT_TEXTO,"';\n
-                    BEGIN
-                      INSERT INTO ADMLEGIS.ITEM_ATO (NUM_ATO, SEQ_ATO, SGL_TIPO, VLR_ANO, SGL_ORGAO, COD_TIPO, DES_ITEM, NUM_LINHA, TXT_TEXTO)
-                          VALUES ('", NUM_ATO,"', '", SEQ_ATO, "', '", SGL_TIPO,
-                              "', '", VLR_ANO, "', '", SGL_ORGAO, "', 'A', 0, 0, TEXTO);
-                    END;")
-  query2 <- paste0("UPDATE ADMLEGIS.ATO SET
-    DTA_PROMULGACAO = TO_DATE('", DTA_PROMULGACAO,"', 'yyyy-mm-dd'),
-    TXT_EMENTA = '", TXT_EMENTA, "',
-    COD_LOCAL = 'BR',
-    DES_TITULO = '", DES_TITULO,"'
-      WHERE NUM_ATO = '", NUM_ATO,"' AND SEQ_ATO = '", SEQ_ATO,
-      "' AND SGL_TIPO = '", SGL_TIPO,"' AND VLR_ANO = '", VLR_ANO,
-      "' AND SGL_ORGAO = '", SGL_ORGAO,"'")
+  query1 <- paste0("INSERT INTO ADMLEGIS.ITEM_ATO (NUM_ATO, SEQ_ATO, SGL_TIPO, VLR_ANO, SGL_ORGAO, COD_TIPO, DES_ITEM, NUM_LINHA, TXT_TEXTO)
+                      VALUES (:1, :2, :3, :4, :5, :6, :7, :8, :9)")
+  query2 <- paste0("UPDATE ADMLEGIS.ATO SET 
+                      DTA_PROMULGACAO = TO_DATE(:1, 'yyyy-mm-dd'),
+                      TXT_EMENTA = :2, COD_LOCAL = :3, DES_TITULO = :4
+                        WHERE NUM_ATO = :5 AND SEQ_ATO = :6 AND SGL_TIPO = :7 AND VLR_ANO = :8 AND SGL_ORGAO = :9")
   query3 <- paste0("INSERT INTO ADMLEGIS.ATO_AGRICULTURA (NUM_ATO, SEQ_ATO, SGL_TIPO, VLR_ANO, SGL_ORGAO)
-    VALUES ('", NUM_ATO, "', '", SEQ_ATO, "', '", SGL_TIPO, "', '", VLR_ANO,"', '", SGL_ORGAO, "')")
+                      VALUES (:1, :2, :3, :4, :5)")
   
-  RJDBC::dbSendUpdate(conexao, query1)
-  RJDBC::dbSendUpdate(conexao, query2)
-  RJDBC::dbSendUpdate(conexao, query3)
+  RJDBC::dbSendUpdate(conexao, query1, NUM_ATO, SEQ_ATO, SGL_TIPO, VLR_ANO, SGL_ORGAO,
+                      COD_TIPO, DES_ITEM, NUM_LINHA, TXT_TEXTO)
+  
+  RJDBC::dbSendUpdate(conexao, query2, DTA_PROMULGACAO, TXT_EMENTA, COD_LOCAL, 
+                      DES_TITULO, NUM_ATO, SEQ_ATO, SGL_TIPO, VLR_ANO, SGL_ORGAO)
+  
+  RJDBC::dbSendUpdate(conexao, query3, NUM_ATO, SEQ_ATO, SGL_TIPO, VLR_ANO, SGL_ORGAO)
+  
   RJDBC::dbCommit(conexao)
+  
   RJDBC::dbDisconnect(conexao)
 }
