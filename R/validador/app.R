4e6cec3d1040f9bcb28e09b72510ee3dbcfcee42
Tomás Barcellos <tomasbarcellos@gmail.com>
2017-10-27 18:09:36 -0200

    Janela do app só abre depois que rodar shiny

--- R/validador/app.R
+++ R/validador/app.R
@@ -1,10 +1,4 @@
-sapply(c('shiny', 'shinyjs', 'httr', 'rJava', 'RJDBC', 'lubridate'), function(pacote) {
-  if (!require(pacote, character.only = TRUE, quietly = TRUE)) {
-    install.packages(pacote, dependencies = TRUE, verbose = FALSE,
-                     repos = 'http://cran.fiocruz.br', quiet = TRUE)
-    require(pacote, character.only = TRUE, quietly = TRUE)
-  }
-})
+﻿sapply(c('shiny', 'shinyjs', 'httr', 'rJava', 'RJDBC', 'lubridate'), require, character.only = TRUE, quietly = TRUE)
 
 options(scipen = 999)
 
--- R/validador/main.js
+++ R/validador/main.js
@@ -1,28 +1,34 @@
+console.log('Inicio do script')
 const electron = require('electron')
 const app = electron.app
 const BrowserWindow = electron.BrowserWindow
-let win;
 
 // rodar shiny
-let spawn = require('child_process').spawn,
-ls = spawn('cmd.exe', ['/c', 'set PATH=%PATH%;C:\Users\%username%\Documents\R\R-3.4.1\bin\i386 & Rscript app.R'])
+const abrirShiny = async () => {
+    console.log('Começa a rodar o shiny')
+    var spawn = require('child_process').spawn
+    var ls = await spawn('cmd.exe', ['/c', 'set PATH=%PATH%;C:\Users\%username%\Documents\R\R-3.4.1\bin\i386 & Rscript app.R'])
+    console.log('ls é:' + ls)
+    
+    ls.stdout.on('data', function (data) {
+        console.log('stdout: ' + data)
+    })
 
-ls.stdout.on('data', function (data) {
-    console.log('stdout: ' + data)
-});
+    ls.stderr.on('data', function (data) {
+        console.log('stderr: ' + data)
+    })
 
-ls.stderr.on('data', function (data) {
-    console.log('stderr: ' + data)
-});
+    ls.on('exit', function (code) {
+        console.log('child process exited with code ' + code)
+    })
+}
 
-ls.on('exit', function (code) {
-    console.log('child process exited with code ' + code)
-});
+abrirShiny()
 
-app.on('ready', async () => {
-    console.log('Esperando rodar o Shiny')
-    await (ms => new Promise(resolve => setTimeout(resolve, ms)))(5000)
-    console.log('Abrindo janela ...')
+let win;
+
+app.on('ready', () => {
+    console.log('Abrindo app...')
     win = new BrowserWindow({
         //frame: false,
         height: 850,
@@ -30,6 +36,8 @@ app.on('ready', async () => {
         width: 1420,
         icon: 'www/icone.ico'
     })
+
+    console.log('Carregando página')
     win.loadURL(`file://${__dirname}/www/index.html`)
 })
 
