da92fa3729082fc40ecd3e53836c54382894e30a
tomasbarcellos <tomasbarcellos@gmail.com>
2017-11-07 07:24:01 -0600

    Corrige pequenas falhas em criar_tabela_app

--- R/rDOU/R/criar_tabela_app.R
+++ R/rDOU/R/criar_tabela_app.R
@@ -10,20 +10,22 @@
 criar_tabela_app <- function(lista_de_atos) {
 
   normas <- tibble::tibble(
-    NUM_ATO = sapply(lista_de_atos, pegar_numero), # Ok
-    SGL_TIPO = sapply(lista_de_atos, pegar_tipo), # Ok
+    NUM_ATO = sapply(lista_de_atos, pegar_numero, USE.NAMES = FALSE), # Ok
+    SGL_TIPO = sapply(lista_de_atos, pegar_tipo, USE.NAMES = FALSE), # Ok
     VLR_ANO = attr(lista_de_atos, 'data_dou') %>% lubridate::year() %>% as.character(), # Deriva de DTA_PROMULGACAO
     SGL_ORGAO = attr(lista_de_atos, 'orgao'), # Ok
-    COD_TIPO = sapply(lista_de_atos, pegar_tipo, 'cod'), # tem que derivar do tipo
-    TXT_TEXTO = sapply(lista_de_atos, paste, collapse = "\n"), # Ok
+    COD_TIPO = sapply(lista_de_atos, pegar_tipo, 'cod', USE.NAMES = FALSE), # tem que derivar do tipo
+    TXT_TEXTO = sapply(lista_de_atos, paste, collapse = "\n", USE.NAMES = FALSE), # Ok
     DTA_PROMULGACAO = attr(lista_de_atos, 'data_dou'), # Ok
-    TXT_EMENTA = sapply(lista_de_atos, pegar_resumo), # A principio fora
-    DES_TITULO = sapply(lista_de_atos, pegar_titulo),
-    NUM_PAGINA = sapply(lista_de_atos, pegar_pagina, attr(lista_de_atos, 'arquivos')),
+    TXT_EMENTA = sapply(lista_de_atos, pegar_resumo, USE.NAMES = FALSE), # A principio fora
+    DES_TITULO = sapply(lista_de_atos, pegar_titulo, USE.NAMES = FALSE),
+    NUM_PAGINA = sapply(lista_de_atos, pegar_pagina, attr(lista_de_atos, 'arquivos'), USE.NAMES = FALSE),
     ID_TIPO_SECAO = attr(lista_de_atos, 'secao')
   )
 
-  remover <- c(grep("PORTARIAS", normas$DES_TITULO), grep("DECISÕES", normas$DES_TITULO))
+  remover <- c(grep("PORTARIAS", normas$DES_TITULO),
+               grep("DECISÕES", normas$DES_TITULO),
+               grep("RETIFICAÇÕES", normas$DES_TITULO))
 
   multiplas <- lapply(normas$TXT_TEXTO[remover], function(x) {
     stringr::str_split(x, "\n")[[1]]
@@ -47,19 +49,19 @@ criar_tabela_app <- function(lista_de_atos) {
 
   repete_dado <- function(variavel) {
     res <- mapply(rep, normas[[variavel]][remover], each = tamanhos, USE.NAMES = FALSE)
-    res[, 1]
+    as.vector(res)
   }
 
   novas_obs <- tibble::tibble(
-    NUM_ATO = repete_dado("NUM_ATO"),
-    SGL_TIPO = repete_dado("SGL_TIPO"),
+    NUM_ATO = sapply(novas_vetor, pegar_numero, USE.NAMES = FALSE),
+    SGL_TIPO = sapply(novas_vetor, pegar_tipo, USE.NAMES = FALSE),
     VLR_ANO = repete_dado("VLR_ANO"),
     SGL_ORGAO = repete_dado("SGL_ORGAO"),
-    COD_TIPO = repete_dado("COD_TIPO"),
+    COD_TIPO = sapply(novas_vetor, pegar_tipo, 'cod', USE.NAMES = FALSE),
     TXT_TEXTO = novas_vetor,
     DTA_PROMULGACAO = as.Date(repete_dado("DTA_PROMULGACAO"), origin = "1970-01-01"),
-    TXT_EMENTA = sapply(novas_vetor, pegar_resumo),
-    DES_TITULO = sapply(novas_vetor, pegar_titulo),
+    TXT_EMENTA = sapply(novas_vetor, pegar_resumo, USE.NAMES = FALSE),
+    DES_TITULO = sapply(novas_vetor, pegar_titulo, USE.NAMES = FALSE),
     NUM_PAGINA = repete_dado("NUM_PAGINA"),
     ID_TIPO_SECAO = repete_dado("ID_TIPO_SECAO")
   )
