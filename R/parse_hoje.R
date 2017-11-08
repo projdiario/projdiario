c5a12e6456485a893edc0f3e3209bb195f7c3c1d
projdiario <tomas.barcellos@agricultura.gov.br>
2017-11-08 15:20:55 -0200

    Muda parse_hoje.R para incluir período e para de acopanhar parser_DOU.R

--- R/parse_hoje.R
+++ R/parse_hoje.R
@@ -1,20 +1,29 @@
 rm(list=ls())
 options()$restart()
 cat('\014')
+devtools::install('R/rDOU')
 library(rDOU)
 
-hoje <- Sys.Date() 
+periodo <- 1 # altere para fazer mais dias
+hoje <- Sys.Date() - (seq_len(periodo) - 1)
+hoje <- hoje[! format(hoje, '%a') %in% c("sáb", "dom")]
 ano <- format(hoje, format = "%Y")
 mes <- format(hoje, format = "%B")
 dia <- format(hoje, format = "%d")
 
-pastas <- paste(paste0('\\\\hp64957/Base SISLEGIS/SISLEGIS/dados/txt/DOU', 1:3), ano, mes, dia,sep = '/')
+pastas <- character(2 * length(hoje))
+
+for (i in seq_along(hoje)) {
+  pastas[c(i, i + length(hoje))] <- paste(paste0('\\\\hp64957/Base SISLEGIS/SISLEGIS/dados/txt/DOU', 1:2),
+                  ano[i], mes[i], dia[i], sep = '/')
+}
+
 
 # Criar driver de conexão e abrir conexão
 driver <- RJDBC::JDBC("oracle.jdbc.OracleDriver", "R/validador/www/ojdbc6.jar")
 configs <- readLines('R/validador/www/config_oracle')
 conexao <- RJDBC::dbConnect(driver, configs[1], configs[2], configs[3])
 
-normas <- lapply(pastas, parsear_e_escrever, conexao = conexao)
+normas <- lapply(pastas, purrr::safely(parsear_e_escrever), conexao = conexao)
 
 RJDBC::dbDisconnect(conexao)
