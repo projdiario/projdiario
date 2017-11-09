39d5fd432e240b19cdca7d223b1e7c885253ccde
projdiario <tomas.barcellos@agricultura.gov.br>
2017-11-09 13:37:44 -0200

    Inclui caso em que não há  normais multiplas

--- R/rDOU/R/criar_tabela_app.R
+++ R/rDOU/R/criar_tabela_app.R
@@ -27,46 +27,47 @@ criar_tabela_app <- function(lista_de_atos) {
                grep("DECISÕES", normas$DES_TITULO),
                grep("RETIFICAÇÕES", normas$DES_TITULO))
 
-  multiplas <- lapply(normas$TXT_TEXTO[remover], function(x) {
-    stringr::str_split(x, "\n")[[1]]
-  })
-
-  novas <- lapply(multiplas, multipla_para_individualizada)
-
-  tam_novas <- sapply(novas, length)
-
-  remover <- remover[tam_novas >= 1]
-
-  novas <- novas[tam_novas >= 1]
-
-  novas_vetor <- unlist(novas)
-
-  numero <- stringr::str_extract(novas_vetor, "Nº [0-9]{1,3}\\.?[0-9]{0,3}") %>%
-    stringr::str_replace_all("Nº ", "") %>% stringr::str_replace_all("\\.", "") %>%
-    as.numeric()
-
-  tamanhos <- sapply(novas, length)
-
-  repete_dado <- function(variavel) {
-    res <- purrr::map2(normas[[variavel]][remover], tamanhos, ~ rep(.x, each = .y))
-    unlist(res)
+  if (length(remover) > 0) {
+    multiplas <- lapply(normas$TXT_TEXTO[remover], function(x) {
+      stringr::str_split(x, "\n")[[1]]
+    })
+    
+    novas <- lapply(multiplas, multipla_para_individualizada)
+    
+    tam_novas <- sapply(novas, length)
+    
+    remover <- remover[tam_novas >= 1]
+    
+    novas <- novas[tam_novas >= 1]
+    
+    novas_vetor <- unlist(novas)
+    
+    tamanhos <- sapply(novas, length)
+    
+    repete_dado <- function(variavel) {
+      res <- purrr::map2(normas[[variavel]][remover], tamanhos, ~ rep(.x, each = .y))
+      unlist(res)
+    }
+    
+    novas_obs <- tibble::tibble(
+      NUM_ATO = sapply(novas_vetor, pegar_numero, USE.NAMES = FALSE),
+      SGL_TIPO = sapply(novas_vetor, pegar_tipo, USE.NAMES = FALSE),
+      VLR_ANO = repete_dado("VLR_ANO"),
+      SGL_ORGAO = repete_dado("SGL_ORGAO"),
+      COD_TIPO = sapply(novas_vetor, pegar_tipo, 'cod', USE.NAMES = FALSE),
+      TXT_TEXTO = novas_vetor,
+      DTA_PROMULGACAO = as.Date(repete_dado("DTA_PROMULGACAO"), origin = "1970-01-01"),
+      TXT_EMENTA = sapply(novas_vetor, pegar_resumo, USE.NAMES = FALSE),
+      DES_TITULO = sapply(novas_vetor, pegar_titulo, USE.NAMES = FALSE),
+      NUM_PAGINA = repete_dado("NUM_PAGINA"),
+      ID_TIPO_SECAO = repete_dado("ID_TIPO_SECAO")
+    )
+    
+    res <- dplyr::bind_rows(normas[-remover, ], novas_obs)
+  } else {
+    res <- normas
   }
 
-  novas_obs <- tibble::tibble(
-    NUM_ATO = sapply(novas_vetor, pegar_numero, USE.NAMES = FALSE),
-    SGL_TIPO = sapply(novas_vetor, pegar_tipo, USE.NAMES = FALSE),
-    VLR_ANO = repete_dado("VLR_ANO"),
-    SGL_ORGAO = repete_dado("SGL_ORGAO"),
-    COD_TIPO = sapply(novas_vetor, pegar_tipo, 'cod', USE.NAMES = FALSE),
-    TXT_TEXTO = novas_vetor,
-    DTA_PROMULGACAO = as.Date(repete_dado("DTA_PROMULGACAO"), origin = "1970-01-01"),
-    TXT_EMENTA = sapply(novas_vetor, pegar_resumo, USE.NAMES = FALSE),
-    DES_TITULO = sapply(novas_vetor, pegar_titulo, USE.NAMES = FALSE),
-    NUM_PAGINA = repete_dado("NUM_PAGINA"),
-    ID_TIPO_SECAO = repete_dado("ID_TIPO_SECAO")
-  )
-
-  res <- dplyr::bind_rows(normas[-remover, ], novas_obs)
   res$TXT_TEXTO <- res$TXT_TEXTO %>% sapply(texto_para_html)
   res
 }
