b5dbb2ee517789e8150f16f1fe2ea145c1976de4
projdiario <tomas.barcellos@agricultura.gov.br>
2017-11-16 18:21:58 -0200

    Inclui tabela com sigla dos órgãos e suas descrições, além de sua documentação

--- R/rDOU/data-raw/dicionarios.R
+++ R/rDOU/data-raw/dicionarios.R
@@ -8,3 +8,8 @@ devtools::use_data(dic_tipos)
 devtools::use_data(dic_tipos,
                    compress = tools::checkRdaFiles("data/dic_tipos.rda")$compress,
                    overwrite = TRUE)
+dic_orgaos <- readxl::read_excel('inst/extdata/des_orgao.xlsx')
+devtools::use_data(dic_orgaos)
+devtools::use_data(dic_orgaos,
+                   compress = tools::checkRdaFiles("data/dic_orgaos.rda")$compress,
+                   overwrite = TRUE)
