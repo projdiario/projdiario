0cfde80d174c853507de233413467e74960241f6
tomasbarcellos <tomasbarcellos@gmail.com>
2018-05-11 14:40:13 -0500

    Alterações nas funções do pacote para ficarem mais no espírito "tidyverse"

--- R/rDOU/R/data.R
+++ R/rDOU/R/data.R
@@ -1,14 +1,3 @@
-#' Lista de tags usadas nos scripts.
-#'
-#' Um data frame com as tags usadas em loops dos scripts
-#'
-#' @format Um data frame com 1 campo e 4 observações:
-#' \describe{
-#'   \item{tag}{nome da tag}
-#' }
-#' @source W3
-"auxiliar"
-
 #' Dicionário dos tipos de normas
 #'
 #' Um data frame com os codigos dos tipos de normas
